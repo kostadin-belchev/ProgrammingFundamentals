674219d02bca61587cdad921dc8167d859cc172c
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-09-30 09:48:38 +0300

    Added in 1 line on Task1

---
+++ Session9/Task 1 Centuries through Minutes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_Centuries_through_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+        }
+    }
+}

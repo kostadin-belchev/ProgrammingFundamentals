160dd4b1a6cfbb8238e5253c545a2ed10661a4a9
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-04 19:00:43 +0300

    Finished Data types and variables LAB

---
+++ 04_Data types and Variables Lab/Task 9 Refractor Special Num/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9_Refractor_Special_Num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            bool isSpecial = false;
+
+            for (int i = 1; i <= num; i++)
+            {
+                int sum = 0;
+                int currentIterNum = i;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+                isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{currentIterNum} -> {isSpecial}");
+                sum = 0;
+                i = currentIterNum;
+            }
+        }
+    }
+}
+
+
+

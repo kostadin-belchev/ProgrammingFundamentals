a5aba70cc4b526494ebc1352da5067c7c32f002a
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-04 17:38:41 +0300

    Changin names of all folders

---
+++ 01_Basic Syntax Exercises/Task 4 Beverage Labels/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4_Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int kCalPer100ml = int.Parse(Console.ReadLine());
+            int gramsOfSugerPer100ml = int.Parse(Console.ReadLine());
+
+            double kcal = kCalPer100ml * volume / 100.0;
+            double sugers = gramsOfSugerPer100ml * volume / 100.0;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{kcal}kcal, {sugers}g sugars");
+        }
+    }
+}
---
+++ 01_Basic Syntax Exercises/Task 5 Character Stats/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5_Character_Stats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            string currHealthString = new string('|', currHealth);
+            string remHealthString = new string('.', (maxHealth - currHealth));
+            Console.WriteLine("Health: |" + currHealthString + remHealthString + "|");
+
+            string currEnergyString = new string('|', currEnergy);
+            string remEnergyString = new string('.', (maxEnergy - currEnergy));
+            Console.WriteLine("Energy: |" + currEnergyString + remEnergyString + "|");
+        }
+    }
+}

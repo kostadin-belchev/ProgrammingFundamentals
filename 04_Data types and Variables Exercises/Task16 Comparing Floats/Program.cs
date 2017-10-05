a5359e81f631b2e76459a8724228781eb0225ea0
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-05 18:05:06 +0300

    Finishing up voluntary tasks

--- 04_Data types and Variables Exercises/Task16 Comparing Floats/Program.cs
+++ 04_Data types and Variables Exercises/Task16 Comparing Floats/Program.cs
@@ -10,6 +10,19 @@ namespace Task16_Comparing_Floats
     {
         static void Main(string[] args)
         {
+            double floatNum1 = double.Parse(Console.ReadLine());
+            double floatNum2 = double.Parse(Console.ReadLine());
+
+            double diff = floatNum1 - floatNum2;
+            if (diff < 0)
+                diff = diff * (-1F);
+
+            double eps = 0.000001F;
+
+            if (diff > eps)
+                Console.WriteLine("False");
+            else
+                Console.WriteLine("True");
         }
     }
 }
--- 04_Data types and Variables Exercises/Task18 Different Integers Size/Program.cs
+++ 04_Data types and Variables Exercises/Task18 Different Integers Size/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
+
 
 namespace Task18_Different_Integers_Size
 {
@@ -10,6 +12,69 @@ namespace Task18_Different_Integers_Size
     {
         static void Main(string[] args)
         {
+            BigInteger bigNumber = new BigInteger();
+            bigNumber = BigInteger.Parse(Console.ReadLine());
+
+
         }
     }
 }
+/*
+             * bool isSByte = false;
+            bool isByte = false;
+            bool isShort = false;
+            bool isUShort = false;
+            bool isInt = false;
+            bool isUInt = false;
+            bool isLong = false;
+            bool isULong = false;
+
+            
+            if (bigNumber <= -9223372036854775808 || bigNumber >= 9223372036854775807)
+            {
+                Console.WriteLine($"{bigNumber} can't fit in any type");
+            }
+            else
+            {
+                Console.WriteLine($"{bigNumber} can fit in:");
+                if (bigNumber >= -128 && bigNumber <= 127)
+                {
+                    isSByte = true;
+                    Console.WriteLine("* sbyte");
+                }
+                if (bigNumber >= 0 && bigNumber <= 255)
+                {
+                    isByte = true;
+                    Console.WriteLine("* byte");
+                }
+                if (bigNumber >= -32768 && bigNumber <= 32767)
+                {
+                    Console.WriteLine("* short");
+                    isShort = true;
+                }
+                if (bigNumber >= 0 && bigNumber <= 65535)
+                {
+                    isUShort = true;
+                    Console.WriteLine("* ushort");
+                }
+                if (bigNumber >= -2147483648 && bigNumber <= 2147483647)
+                {
+                    isInt = true;
+                    Console.WriteLine("* int");
+                }
+                if (bigNumber >= 0 && bigNumber <= 4294967295)
+                {
+                    isUInt = true;
+                    Console.WriteLine("* uint");
+                }
+                if (bigNumber >= -9223372036854775808 && bigNumber <= 9223372036854775807)
+                {
+                    isLong = true;
+                    Console.WriteLine("* long");
+                }
+            }
+             * if (bigNumber >= 0 && bigNumber <= 18446744073709551615)
+            {
+                isULong = true;
+                Console.WriteLine("* ulong");
+            }*/

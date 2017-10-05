3ab9b0b1f6180ea533846a39670d581ea8dea60e
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-05 20:00:17 +0300

    Did 2 problems from lab

---
+++ 05_Metods Lab/Session11/02_Sign of Integer Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Sign_of_Integer_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintSign(n);
+        }
+
+        static void PrintSign(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+
+        }
+    }
+}

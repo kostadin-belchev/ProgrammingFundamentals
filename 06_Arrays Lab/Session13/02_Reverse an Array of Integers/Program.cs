3c7c49e237e62f025a25c23af723598a07574bd6
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-12 14:24:48 +0300

    Did some Lab tasks

---
+++ 06_Arrays Lab/Session13/02_Reverse an Array of Integers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Reverse_an_Array_of_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arrayOfInts = new int[n];
+
+            for (int i = 0; i < arrayOfInts.Length; i++)
+            {
+                arrayOfInts[i] = int.Parse(Console.ReadLine());
+            }
+
+            //int[] reversedArrayOfInts = new int[n];
+
+            for (int i = arrayOfInts.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arrayOfInts[i] + " ");
+            }
+
+        }
+    }
+}
---
+++ 06_Arrays Lab/Session13/08_Condense Array to Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(new[] { ' ' },StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int finalResult = 0;
+
+            for (int j = 0; j < nums.Length - 1; j++)
+            {
+                int[] condensed = new int[nums.Length - 1];
+
+                for (int i = 0; i < condensed.Length; i++)
+                {
+                    condensed[i] = nums[i] + nums[i + 1];
+                }
+                nums = condensed;
+                //Array.Resize(ref nums, nums.Length - 1);
+                finalResult = condensed[0];
+            }
+            Console.WriteLine(finalResult);
+        }
+    }
+}

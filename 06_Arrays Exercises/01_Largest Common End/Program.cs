b24785c1af405230920638a0d7b913060c7bb199
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-14 09:38:41 +0300

    v1

---
+++ 06_Arrays Exercises/01_Largest Common End/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Largest_Common_End
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstStringOfWords = Console.ReadLine().Split(' ').ToArray();
+            string[] secondStringOfWords = Console.ReadLine().Split(' ').ToArray();
+
+            int shorterArrayLenght = Math.Min(firstStringOfWords.Length, secondStringOfWords.Length);
+            int longerArrayLenght = Math.Max(firstStringOfWords.Length, secondStringOfWords.Length);
+
+            int rightCommonEnd = 0;
+            int leftCommonEnd = 0;
+
+            for (int i = 0; i < shorterArrayLenght; i++)
+            {
+                if (firstStringOfWords[i] == secondStringOfWords[i])
+                {
+                    leftCommonEnd++;
+                }
+            }
+
+            for (int i = 0; i < shorterArrayLenght; i++)
+            {
+                if (firstStringOfWords[firstStringOfWords.Length - 1 - i] == secondStringOfWords[secondStringOfWords.Length - 1 - i])
+                {
+                    rightCommonEnd++;
+                }
+            }
+            Console.WriteLine(Math.Max(rightCommonEnd, leftCommonEnd));
+        }
+    }
+}
---
+++ 06_Arrays Exercises/02_Rotate and Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Rotate_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int k = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}

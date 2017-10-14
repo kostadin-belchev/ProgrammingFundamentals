7d62fb071b3551c7acdcf3c70a7af3d1760a8c2b
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-14 09:40:50 +0300

    arranging

---
+++ 06_Arrays Lab/03_Last K Numbers Sums Sequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Last_K_Numbers_Sums_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            int[] seq = new int[n];
+
+            seq[0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i - k; j < i - 1; j++)
+                {
+                    
+                }
+            }
+        }
+    }
+}

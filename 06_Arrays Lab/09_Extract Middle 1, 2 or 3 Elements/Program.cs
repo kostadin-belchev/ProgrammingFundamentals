d9bd49593b613aea8cc638fc3e461272a84e16e2
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-15 01:13:26 +0300

    finsished array lab 1h12 in the moring on a saturday night :D

---
+++ 06_Arrays Lab/09_Extract Middle 1, 2 or 3 Elements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Extract_Middle_1__2_or_3_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            if (n.Length == 1)
+                Console.WriteLine($"{{ {n[0]} }}");
+            else if (n.Length % 2 == 0)
+            {
+                 Console.WriteLine($"{{ {n[n.Length / 2 - 1]}, {n[n.Length/2]} }}");
+            }
+            else if (n.Length % 2 != 0)
+            {
+                Console.WriteLine($"{{ {n[n.Length / 2 - 1]}, {n[n.Length / 2]}, {n[n.Length/2 + 1]} }}");
+            }
+        }
+    }
+}

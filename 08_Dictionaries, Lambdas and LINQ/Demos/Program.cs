340bf325320d803eb5a209a8c206b26424bf646f
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-15 21:10:35 +0300

    added in some demos, all lab is finished though

---
+++ 08_Dictionaries, Lambdas and LINQ/Demos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6 };
+
+            int firstNum = nums.First(); // 1
+            Console.WriteLine(firstNum);
+
+            int lastOddNum = nums.Last(x => x % 2 == 1); // 5
+            Console.WriteLine(lastOddNum);
+
+            int singleNum = nums.Single(x => x == 4); // 4
+            Console.WriteLine(singleNum);
+        }
+    }
+}

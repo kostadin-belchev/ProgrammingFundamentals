e766143e9410041a9730bb94dbd5289240894f5c
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-15 21:57:25 +0300

    did first exercise, only one functionality is missing, replacing old number with new

---
+++ 08_Dictionaries, Lambdas and LINQ Exercises/01_Phonebook/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+            string[] commands = Console.ReadLine().Split(' ').ToArray();
+
+            while (commands[0] != "END")
+            {
+                string currKey = commands[1];
+
+                if (commands[0] == "A")
+                {
+                    phonebook.Add(commands[1], commands[2]);
+                }
+                else if (commands[0] == "S")
+                {
+                    if (phonebook.ContainsKey(commands[1]))
+                    {
+                        Console.WriteLine($"{commands[1]} -> {phonebook[commands[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {commands[1]} does not exist.");
+                    }
+                }
+                commands = Console.ReadLine().Split(' ').ToArray();
+            }
+        }
+    }
+}

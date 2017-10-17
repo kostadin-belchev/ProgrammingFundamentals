8cd74c90f65c72968d6b854494de1ec1c7ae6e78
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-17 23:11:09 +0300

    only 1 task left from lab of Lists

--- 07_Lists Lab/07_Count Numbers/Program.cs
+++ 07_Lists Lab/07_Count Numbers/Program.cs
@@ -10,6 +10,25 @@ namespace _07_Count_Numbers
     {
         static void Main(string[] args)
         {
+            // Algorithm 1: Sort the numbers and count occurrences of each number.
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Array.Sort(nums);
+
+            //Test if sorted correctly
+            //Console.WriteLine(string.Join(" ", nums));
+
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                var counter = 1;
+                if (nums[i - 1] == nums[i])
+                {
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine($"{nums[i]} -> {counter}");
+                }
+            }
         }
     }
 }

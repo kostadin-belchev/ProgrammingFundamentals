7432635235aa6feb8aefa44c1d2dd3c04b3f9e62
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-18 12:07:09 +0300

    finished all exercises wihtout start sign, meaning all mandatory

--- 07_Lists Exercises/03_Search for a Number/Program.cs
+++ 07_Lists Exercises/03_Search for a Number/Program.cs
@@ -10,7 +10,25 @@ namespace _03_Search_for_a_Number
     {
         static void Main(string[] args)
         {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> threeNums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> resultingArray = new List<int>();
+
+            int num1SecondLine = threeNums[0];
+            int num2SecondLine = threeNums[1];
+            int num3SecondLine = threeNums[2];
+
+            resultingArray = nums.Take(num1SecondLine).ToList();
+
+            resultingArray.RemoveRange(0, num2SecondLine);
+
             
+            if (resultingArray.Contains(num3SecondLine))
+                Console.WriteLine("YES!");
+            else
+                Console.WriteLine("NO!");
         }
     }
 }

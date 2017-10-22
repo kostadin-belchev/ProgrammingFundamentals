6623d0b57ff79eaad9523be581c60592938ab098
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-22 10:27:27 +0300

    finished lab for objects and classes

--- 09_Objects and Classes/06_Rectangle Position/Program.cs
+++ 09_Objects and Classes/06_Rectangle Position/Program.cs
@@ -10,6 +10,59 @@ namespace _06_Rectangle_Position
     {
         static void Main(string[] args)
         {
+            Rectangle r1 = ReadRectangle();
+            Rectangle r2 = ReadRectangle();
+
+            if (r1.IsInside(r2))
+                Console.WriteLine("Inside");
+            else
+                Console.WriteLine("Not inside");
+        }
+
+        public static Rectangle ReadRectangle()
+        {
+            int[] leftTopWidthHeight = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Rectangle r = new Rectangle() { Left = leftTopWidthHeight.First(), Top = leftTopWidthHeight.Skip(1).First(), Width = leftTopWidthHeight.Skip(2).First(), Height = leftTopWidthHeight.Last() };
+            return r;
+        }
+
+        public class Rectangle
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public int Botton
+            {
+                get
+                {
+                    return Top + Height;
+                }
+            }
+
+            public int Right
+            {
+                get
+                {
+                    return Left + Width;
+                }
+            }
+
+            
+
+            public bool IsInside(Rectangle other) ////What out with the private statement
+            {
+                var isInLeft = this.Left >= other.Left;
+                var isInRight = this.Right <= other.Right;
+                var isInTop = this.Top >= other.Top;
+                var isInBottom = this.Botton <= other.Botton;
+
+                var isInsideHorizontal = isInLeft && isInRight;
+                var isInsideVertical = isInTop && isInBottom;
+
+                return isInsideHorizontal && isInsideVertical;
+            }
         }
     }
 }
---
+++ 09_Objects and Classes/07_Sales Report/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Sales_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Sale[] sales = new Sale[n];
+
+            for (int i = 0; i < sales.Length; i++)
+                sales[i] = ReadSale();
+
+            SortedDictionary<string, decimal> salesByTown = new SortedDictionary<string, decimal>();
+
+            foreach (var sale in sales)
+            {
+                decimal totalSales = sale.Price * sale.Quantity;
+                if (salesByTown.ContainsKey(sale.Town))
+                {
+                    salesByTown[sale.Town] += totalSales;
+                }
+                else
+                {
+                    salesByTown.Add(sale.Town, totalSales);
+                }
+            }
+            foreach (var keyvaluePair in salesByTown)
+            {
+                Console.WriteLine($"{keyvaluePair.Key} -> {keyvaluePair.Value:F2}");
+            }
+        }
+
+        public static Sale ReadSale()
+        {
+            string[] input = Console.ReadLine().Split();
+            Sale sale = new Sale() { Town = input[0], Product = input[1], Price = decimal.Parse(input[2]), Quantity = decimal.Parse(input[3]) };
+            return sale;
+        }
+
+        public class Sale
+        {
+            public string Town { get; set; }
+            public string Product { get; set; }
+            public decimal Price { get; set; }
+            public decimal Quantity { get; set; }
+
+        }
+    }
+}

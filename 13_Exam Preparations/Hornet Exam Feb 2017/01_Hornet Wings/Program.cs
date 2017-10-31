1058bcd4fbbe3db2cf44ea7e1baba31508280899
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-31 20:16:19 +0200

    started hornet exam

---
+++ 13_Exam Preparations/Hornet Exam Feb 2017/01_Hornet Wings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Hornet_Wings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wingFlapsN = int.Parse(Console.ReadLine());
+            double metersFor1000FlapsM = double.Parse(Console.ReadLine());
+            int enduranceP = int.Parse(Console.ReadLine());
+
+            double metersTraveled = (wingFlapsN / 1000D) * metersFor1000FlapsM;
+            double intermTime = wingFlapsN / 100D;
+            double secondsPassed = intermTime + (wingFlapsN / enduranceP) * 5D;
+
+            Console.WriteLine($"{metersTraveled:F2} m.");
+            Console.WriteLine($"{secondsPassed} s.");
+        }
+    }
+}
---
+++ 13_Exam Preparations/Hornet Exam Feb 2017/02_Hornet Comm/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _02_Hornet_Comm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<string> inputLines = new List<string>();
+
+            while (input != "Hornet is Green")
+            {
+                if (input == "Hornet is Green")
+                    break;
+                else
+                {
+                    inputLines.Add(input);
+                }
+                input = Console.ReadLine();
+            }
+            //Console.WriteLine(string.Join(", ", inputLines));
+
+            Dictionary<string, string> firstQAndSecondQ = new Dictionary<string, string>();
+
+            foreach (var line in inputLines)
+            {
+                string[] elementsOfLine = Regex.Split(line, " <-> ");
+                var keyToAdd = elementsOfLine[0];
+                var valueToAdd = elementsOfLine[1];
+
+                if (!firstQAndSecondQ.ContainsKey(keyToAdd))
+                    firstQAndSecondQ.Add(keyToAdd, valueToAdd);
+            }
+
+            //bool isDigit = 
+            
+        }
+    }
+}

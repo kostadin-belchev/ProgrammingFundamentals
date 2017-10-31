39f16e71a9c7741934ed8a7738e0c6de2d279ee8
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-10-31 17:35:02 +0200

    second to last task is in progress

--- 13_Exam Preparations/Exam Preparation III/03_Rage Quit/Program.cs
+++ 13_Exam Preparations/Exam Preparation III/03_Rage Quit/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _03_Rage_Quit
@@ -10,6 +11,25 @@ namespace _03_Rage_Quit
     {
         static void Main(string[] args)
         {
+            string pattern = @"([^0-9]+)(\d+)";
+            string input = Console.ReadLine();
+
+            StringBuilder rageMessage = new StringBuilder();
+
+            foreach (Match m in Regex.Matches(input, pattern))
+            {
+                var word = m.Groups[1].Value.ToUpper();
+                var count = int.Parse(m.Groups[2].Value);
+
+                for (int i = 0; i < count; i++)
+                {
+                    rageMessage.Append(word);
+                }
+            }
+
+            int uniqueSymbols = rageMessage.ToString().Distinct().Count();
+            Console.WriteLine($"Unique symbols used: {uniqueSymbols}");
+            Console.WriteLine(rageMessage);
         }
     }
 }
---
+++ 13_Exam Preparations/Exam Preparation III/04_Files/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            long N = long.Parse(Console.ReadLine());
+            Dictionary<string, decimal> pathAndSize = new Dictionary<string, decimal>();
+            Dictionary<string, decimal> nameOfExtensionAndSize = new Dictionary<string, decimal>();
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] currPathAndSize = Console.ReadLine().Split(';').ToArray();
+                pathAndSize.Add(currPathAndSize[0], decimal.Parse(currPathAndSize[1]));
+            }
+
+            List<string> paths = new List<string>();
+            foreach (var path in pathAndSize)
+            {
+                paths = path.Key.Split('\\').ToList();
+            }
+
+            string[] searchItems = Console.ReadLine().Split().ToArray();
+            string extension = searchItems[0];
+            string root = searchItems[2];
+
+            //program
+            foreach (var keyValuePair in nameOfExtensionAndSize)
+            {
+                
+            }
+
+            //output
+        }
+    }
+}

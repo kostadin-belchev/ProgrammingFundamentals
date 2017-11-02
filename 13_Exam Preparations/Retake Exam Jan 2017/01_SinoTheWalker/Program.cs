8eb0bc894387b5021c9b56637182f22714841514
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-11-02 14:12:53 +0200

    doing 4th task

---
+++ 13_Exam Preparations/Retake Exam Jan 2017/01_SinoTheWalker/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class SinoTheWalker
+{
+    static void Main()
+    {
+        //input
+        string leaveTime = Console.ReadLine();
+        string[] leaveTimeTokens = leaveTime.Split(':').ToArray();
+        int leaveHours = int.Parse(leaveTimeTokens[0]);
+        int leaveMinutes = int.Parse(leaveTimeTokens[1]);
+        int leaveSeconds = int.Parse(leaveTimeTokens[2]);
+
+        int numberOfSteps = int.Parse(Console.ReadLine()) % 86400;
+
+        int secondsPerEachStep = int.Parse(Console.ReadLine()) % 86400;
+
+        //program
+        long totalSeconsToAdd = numberOfSteps * secondsPerEachStep;
+
+        long arriveSeconds = leaveSeconds + totalSeconsToAdd;
+        long arriveMinutes = leaveMinutes;
+        long arriveHours = leaveHours;
+
+        while (arriveSeconds > 59)
+        {
+            arriveMinutes++;
+            arriveSeconds -= 60;
+        }
+        //Console.WriteLine(arriveMinutes);
+        while (arriveMinutes > 59)
+        {
+            arriveHours++;
+            arriveMinutes -= 60;
+        }
+        //arriveHours %= 24;
+        while (arriveHours > 23)
+        {
+            arriveHours -= 24;
+        }
+
+        //output
+        Console.WriteLine($"Time Arrival: {arriveHours:D2}:{arriveMinutes:D2}:{arriveSeconds:D2}");
+    }
+}
+
+//DateTime timeOfLeavingSoftUni = DateTime.Parse(Console.ReadLine());
+////int step = int.Parse(Console.ReadLine()) % 86400;
+////int seconds = int.Parse(Console.ReadLine()) % 86400;
+////86400 секунди = 1 ден
+////махаме цели дни, ако някой се прави на интересен да ни ги подава като вход
+//int stepsTaken = int.Parse(Console.ReadLine()) % 86400;
+//int stepsPerSec = int.Parse(Console.ReadLine()) % 86400;
+//int timeInTravel = stepsTaken * stepsPerSec;
+//DateTime result = timeOfLeavingSoftUni.AddSeconds(timeInTravel);
+////Output
+//Console.WriteLine("Time Arrival: {0:HH:mm:ss}", result);
+
---
+++ 13_Exam Preparations/Retake Exam Jan 2017/03_EnduranceRally/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class EnduranceRally
+{
+    static void Main()
+    {
+        //input
+        string driversNames = Console.ReadLine();
+        string zones = Console.ReadLine();
+        string checkpoints = Console.ReadLine();
+
+        List<string> listOfDrivers = driversNames.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        double[] listOfZones = zones.Split(' ').Select(double.Parse).ToArray();
+        List<int> checkpointIndexes = checkpoints.Split(' ').Select(int.Parse).ToList();
+
+        //program
+
+        Dictionary<string, double> driversFuel = new Dictionary<string, double>();
+
+        foreach (var driver in listOfDrivers)
+        {
+            var fuel = (double)driver.First();
+            var reached = -1; // or 0.... not sure
+
+            for (int i = 0; i < listOfZones.Length; i++)
+            {
+                var zone = listOfZones[i];
+                
+                if (!IsCheckpiont(zone, checkpointIndexes, listOfZones))
+                    fuel -= zone;
+                else
+                    fuel += zone;
+
+                if (fuel <= 0)
+                {
+                    reached = i;
+                    break;
+                }
+            }
+
+            if (reached == -1)
+            {
+                Console.WriteLine($"{driver} - fuel left {fuel:F2}");
+            }
+            else
+                Console.WriteLine($"{driver} - reached {reached}");
+        }
+    }
+
+    private static bool IsCheckpiont(double zone, List<int> checkpointIndexes, double[] listOfZones)
+    {
+        if (checkpointIndexes.Contains(Array.IndexOf(listOfZones, zone)))
+            return true;
+        else
+            return false;
+    }
+}
+
+//List<int> initialFuels = new List<int>();
+//        foreach (var driver in listOfDrivers)
+//            initialFuels.Add((int) driver.First());
+
+//        Dictionary<string, double> driversFuel = new Dictionary<string, double>();
+
+//        for (int i = 0; i<listOfDrivers.Count; i++)
+//        {
+//            driversFuel.Add(listOfDrivers[i], initialFuels[i]);
+//        }
+
+//        foreach (var df in driversFuel)
+//        {
+//            for (int i = 0; i<listOfZones.Count; i++)
+//            {
+//                if (IsCheckpoint(listOfZones[i]))
+//                    driversFuel[df.Key] += listOfZones[i];
+//                else
+//                    driversFuel[df.Key] -= listOfZones[i];
+//            }
+//        }
+

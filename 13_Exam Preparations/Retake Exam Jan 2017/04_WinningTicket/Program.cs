3b369bb40ca137da207e373a2707acb7034e6384
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-11-04 12:10:34 +0200

    doing some exam tasks

--- 13_Exam Preparations/Retake Exam Jan 2017/04_WinningTicket/Program.cs
+++ 13_Exam Preparations/Retake Exam Jan 2017/04_WinningTicket/Program.cs
@@ -25,26 +25,25 @@ class WinningTicket
                 var matches = Regex.Matches(input, pattern);
                 if (matches.Count > 0)
                 {
-                    Console.WriteLine($"ticket \"{ticket}\" - 10$ Jackpot!");
+                    Console.WriteLine($"ticket \"{ticket}\" - 10{matches[0].Value.First()} Jackpot!");
                 }
-                else
+                else //the code below sould not ne in a else, maybe a if else....
                 {
                     string pattern2 = @"[$]{6,9}|[@]{6,9}|[#]{6,9}|[\^]{6,9}";
-                    string firstHalf = ticket.Take(5).ToString();
-                    string secondHalf = ticket.Skip(5).Take(5).ToString();
+                    string firstHalf = ticket.Substring(0, 10);
+                    string secondHalf = ticket.Substring(10); // or ticket.Substring(10, 20);
+                    var matches2 = Regex.Matches(firstHalf, pattern2);
+                    var matches3 = Regex.Matches(secondHalf, pattern2);
 
-                    foreach (Match mFirst in Regex.Matches(firstHalf, pattern2))
+                    if (matches2.Count > 0 && matches3.Count > 0)
                     {
-                        foreach (Match mSecond in Regex.Matches(secondHalf, pattern2))
-                        {
-                            if (mFirst == mSecond)
-                            {
-                                Console.WriteLine($"ticket \"{ticket}\" - {mFirst.Length}$");
-                            }
-                            else
-                                Console.WriteLine($"ticket \"{ticket}\" - no match");
-                        }
+                        if (matches2[0].Value == matches3[0].Value && firstHalf.Length <= secondHalf.Length)
+                            Console.WriteLine($"ticket \"{ticket}\" - {matches2[0].Length}{matches2[0].Value.First()}");
+                        else if (matches2[0].Value == matches3[0].Value && firstHalf.Length > secondHalf.Length)
+                            Console.WriteLine($"ticket \"{ticket}\" - {matches3[0].Length}{matches3[0].Value.First()}");
                     }
+                    else
+                        Console.WriteLine($"ticket \"{ticket}\" - no match");
                 }
             }
         }
